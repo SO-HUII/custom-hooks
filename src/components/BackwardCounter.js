50075a0cdd3f1588bb5f2d9e631ac08f6a163f5f
SO HUI <haejuuun925@gmail.com>
2023-11-14 21:22:20 +0900

    feat: 커스텀 hook, useCounter을 사용해 카운트 증가, 감소 업데이트

--- src/components/BackwardCounter.js
+++ src/components/BackwardCounter.js
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import Card from './Card';
+import useCounter from './../hooks/use-counter';
 
 const BackwardCounter = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCounter((preCounter) => preCounter - 1);
-        }, 1000);
-
-        return () => clearInterval(interval); 
-    }, []);
+    const counter = useCounter(false);
     
     return (
         <Card>{counter}</Card>
--- src/components/ForwardCounter.js
+++ src/components/ForwardCounter.js
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from 'react';
 import Card from './Card';
+import useCounter from '../hooks/use-counter';
 
 const ForwardCounter = () => {
-    const [counter, setCounter] = useState(0);
-
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCounter((prevCounter) => prevCounter + 1);
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
+    const counter = useCounter();
 
     return <Card>{counter}</Card>;
 };
